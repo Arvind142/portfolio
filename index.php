0fc868fed0690a45bc30875288ff9cbae96d302a
Arvind Choudhary <arvindchoudhary142@yahoo.in>
2021-10-02 12:36:50 +0530

    icons added!

--- index.php
+++ index.php
@@ -100,6 +100,8 @@
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
 					<!-- <li class="nav-item"><a class="nav-link active"
 						aria-current="page">links&gt;</a></li> -->
+
+
 					<li class="nav-item"><a class="nav-link" href="#about">About</a></li>
 					<li class="nav-item"><a class="nav-link" href="#skill">Skill</a></li>
 					<li class="nav-item"><a class="nav-link" href="#experience">Experience</a></li>
@@ -113,180 +115,150 @@
 
 
 	<!-- about -->
-	<div class="card" id="about">
-		<div class="card-header">
-			<h4>About</h4>
+	<div class="row g-0 d-flex p-2 justify-content-evenly" id="about">
+		<nav class="d-flex p-2 justify-content-start border">
+			<span class="mb-0 h3">About</span>
+		</nav>
+		<div class="col-sm-6 col-md-6 d-flex p-2 justify-content-evenly">
+			<p>
+				<img src="images/about.png" width="200" height="200"
+					class="img-fluid" alt="about">
+			</p>
 		</div>
-		<div class="card-body">
-
-			<div class="container">
-				<div class="row justify-content-evenly">
-					<div class="col-4">
-						<h5 class="card-title">Based in Mumbai</h5>
-						<br>
-						<p class="card-text">
-							<img src="images/logo.png" class="img-fluid" alt="Myself">
-						</p>
-					</div>
-					<div class="col-4">
-						<h5 class="card-title">I Code, therefore I am.</h5>
-						<p class="card-text">
-							I am a young enthusiast who is keen on IT and anything about it
-							since my first steps to High School. During the past few years, I
-							gained lots of experience in this field due to the strong desire
-							of becoming an expert. Today I am a senior front-end developer at
-							one of the leading companies in the world. <br> <br> <strong>“For
-								me, web development is more than coding. It's a canvas with a
-								blank page and I am the artist to paint it with colors of
-								coding”.</strong>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- Skill -->
-
-	<div class="card" id="skill">
-		<div class="card-header">
-			<h4>Skill</h4>
-		</div>
-		<div class="card-body">
-
-			<div class="container">
-				<div class="row justify-content-evenly">
-					<div class="col-4">
-						<table class="table table-hover justify-content-evenly">
-							<thead>
-								<tr>
-									<th>No.</th>
-									<th>Name</th>
-									<th>Category</th>
-									<th>Years Of Exp.</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>1.</td>
-									<td>Java</td>
-									<td>Programming</td>
-									<td>3.2</td>
-								</tr>
-
-								<tr>
-									<td>2.</td>
-									<td>Selenium</td>
-									<td>Browser automation</td>
-									<td>3.2</td>
-								</tr>
-								
-								<tr>
-									<td>3.</td>
-									<td>TestNG</td>
-									<td>Testing framework</td>
-									<td>3.2</td>
-								</tr>
-							</tbody>
-						</table>
-
-					</div>
-					<div class="col-4">
-						<h5 class="card-title">I Code, therefore I am.</h5>
-						<p class="card-text">
-							I am a young enthusiast who is keen on IT and anything about it
-							since my first steps to High School. During the past few years, I
-							gained lots of experience in this field due to the strong desire
-							of becoming an expert. Today I am a senior front-end developer at
-							one of the leading companies in the world. <br> <br> <strong>“For
-								me, web development is more than coding. It's a canvas with a
-								blank page and I am the artist to paint it with colors of
-								coding”.</strong>
-						</p>
-					</div>
-				</div>
-			</div>
+		<div class="col-6 col-md-6">
+			<strong>Automation tester with 3.2 years of experience in
+				creation, maintenance &amp; optimization of scripts. Have also
+				worked on maintenance and enhancement of framework.</strong><br> I'm a
+			young enthusiast who is keen on learning new languages/tools/ways to
+			get things done. <br> During the past few years, I gained lots
+			of experience in this field due to the strong desire of becoming an
+			expert. Today I am a Automation Engineer at one of the leading MNC. <br>
+			<br> <b> Few words to describe my work</b>
+			<ul>
+				<li>Automation Tester(Web/Windows/Api/DB)</li>
+				<li>TDD/BDD based script development</li>
+				<li>Agile Tester</li>
+			</ul>
 		</div>
 	</div>
 
 
-	<!-- Experience -->
-	<div class="card" id="experience">
-		<div class="card-header">
-			<h4>Experience</h4>
+	<!-- skill -->
+	<div class="row g-0 d-flex p-2 justify-content-evenly" id="skill">
+		<nav class="d-flex p-2 justify-content-start border">
+			<span class="mb-0 h3">Skill</span>
+		</nav>
+		<div class="col-sm-6 col-md-6 d-flex p-2 justify-content-evenly">
+			<p>
+				<img src="images/skill.png" width="200" height="200"
+					class="img-fluid" alt="skill">
+			</p>
 		</div>
-		<div class="card-body">
+		<div class="col-6 col-md-6">
+			<br> <b> Soft Skills</b>
+			<ul>
+				<li>Quick learner</li>
+				<li>Problem Solving</li>
+				<li>Time management</li>
+				<li>Team player</li>
+				<li>Leadership</li>
+				<li>Flexible</li>
+			</ul>
 
-			<div class="container">
-				<div class="row justify-content-evenly">
-					<div class="col-4">
-						<h5 class="card-title">Based in Mumbai</h5>
-						<br>
-						<p class="card-text">
-							<img src="images/logo.png" class="img-fluid" alt="Myself">
-						</p>
-					</div>
-					<div class="col-4">
-						<h5 class="card-title">I Code, therefore I am.</h5>
-						<p class="card-text">
-							I am a young enthusiast who is keen on IT and anything about it
-							since my first steps to High School. During the past few years, I
-							gained lots of experience in this field due to the strong desire
-							of becoming an expert. Today I am a senior front-end developer at
-							one of the leading companies in the world. <br> <br> <strong>“For
-								me, web development is more than coding. It's a canvas with a
-								blank page and I am the artist to paint it with colors of
-								coding”.</strong>
-						</p>
-					</div>
-				</div>
-			</div>
+			<br> <b> Hard Skills</b>
+			<ul>
+				<li>Programming Language
+					<ul>
+						<li>Java</li>
+						<li>Python</li>
+					</ul>
+				</li>
+				<li>Testing Framework
+					<ul>
+						<li>TestNG</li>
+						<li>JUnit</li>
+					</ul>
+				</li>
+				<li>Automation
+					<ul>
+						<li>Selenium - Browser automation</li>
+						<li>Leanft - Window automation</li>
+						<li>Winnium - Window automation</li>
+						<li>RestAssured - API automation</li>
+					</ul>
+				</li>
+				<li>Cucumber - BDD</li>
+				<li>Git - version control</li>
+				<li>Jenkins - CICD</li>
+				<li>Apache Maven - software project management</li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- experience -->
+	<div class="row g-0 d-flex p-2 justify-content-evenly" id="experience">
+		<nav class="d-flex p-2 justify-content-start border">
+			<span class="mb-0 h3">Experience</span>
+		</nav>
+		<div class="col-sm-6 col-md-6 d-flex p-2 justify-content-evenly">
+			<p>
+				<img src="images/experience.png" width="200" height="200"
+					class="img-fluid" alt="experience">
+			</p>
+		</div>
+		<div class="col-6 col-md-6">
+			<br> <b> Career Path</b>
+			<ul>
+				<li>Infosys Limited
+					<ul>
+						<li>Roles:
+							<ul>
+								<li>Test Engineer [04.2021 - Present]</li>
+								<li>Testing Executive [07.2018 - 03.2021]</li>
+							</ul>
+						</li>
+						<li>Responsibilities:
+							<ol>
+								<li>E2E regression automation</li>
+								<li>Regression code maintenance</li>
+								<li>Code optimization and enhancement</li>
+							</ol>
+						</li>
+					</ul>
+				</li>
+			</ul>
 		</div>
 	</div>
 
 
-	<!-- Contact -->
-
-	<div class="card" id="contact">
-		<div class="card-header">
-			<h4>Contact</h4>
-		</div>
-		<div class="card-body">
-
-			<div class="container">
-				<div class="row justify-content-evenly">
-					<div class="col-4">
-						<h5 class="card-title">Based in Mumbai</h5>
-						<br>
-						<p class="card-text">
-							<img src="images/logo.png" class="img-fluid" alt="Myself">
-						</p>
-					</div>
-					<div class="col-4">
-						<h5 class="card-title">I Code, therefore I am.</h5>
-						<p class="card-text">
-							I am a young enthusiast who is keen on IT and anything about it
-							since my first steps to High School. During the past few years, I
-							gained lots of experience in this field due to the strong desire
-							of becoming an expert. Today I am a senior front-end developer at
-							one of the leading companies in the world. <br> <br> <strong>“For
-								me, web development is more than coding. It's a canvas with a
-								blank page and I am the artist to paint it with colors of
-								coding”.</strong>
-						</p>
-					</div>
-				</div>
-			</div>
+	<!-- contact -->
+	<div class="row g-0 d-flex p-2 justify-content-evenly" id="contact">
+		<nav class="d-flex p-2 justify-content-start border">
+			<span class="mb-0 h3">Contact</span>
+		</nav>
+		<div class="col-6 col-md-6">
+			<table class="table table-borderless">
+				<tr>
+					<td><a class="alert"
+						href="https://www.linkedin.com/in/arvind-choudhary-0b0a82171/" target="_blank"><img
+							src="images/linkedin.gif" class="rounded-circle img-fluid"
+							alt="linkedIn" width="30" height="30"></a></td>
+					<td><a class="alert" href="mailto:arvindchoudhary142@yahoo.in"><img
+							src="images/mailbox.gif" class="rounded-circle img-fluid"
+							alt="mailBox" width="30" height="30"> </a></td>
+				</tr>
+			</table>
 		</div>
 	</div>
+
+
+
+
 
 
 	<!-- go to TOP button -->
 	<button onclick="top_scroll_frame_custom()"
 		class="btn btn-outline-secondary" style="width: 100px;" id="backToTop"
 		title="Go to top">Top</button>
-
-
-
 </body>
 </html>
